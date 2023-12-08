4b9880574ea8f2a9f6151db6f727429f47d038a1
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2023-12-08 01:37:46 +0000

    12-7-23 updates

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let domainNames; // Declare domainNames outside the generateDomain function
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    domainNames = generateDomain();
+    document.querySelector("#the-domain").innerHTML =
+      domainNames[Math.floor(Math.random() * domainNames.length)];
+  });
+
+  let generateDomain = () => {
+    let pronoun = ["the", "our", "your", "its", "my"];
+    let adj = ["great", "big", "happy", "ugly", "slow"];
+    let noun = ["book", "dog", "family", "tree", "time"];
+    let dom = [".com", ".net", ".gov", ".io", ".us"];
+
+    let generatedDomains = [];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < dom.length; l++) {
+            let domainName = `${pronoun[i]}${adj[j]}${noun[k]}${dom[l]}`;
+            generatedDomains.push(domainName);
+          }
+        }
+      }
+    }
+    return generatedDomains;
+  };
 };
